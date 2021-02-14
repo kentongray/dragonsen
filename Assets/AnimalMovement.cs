6908351001d23d16fef9bf20b73c584980973c27
Kenton <kentongray@gmail.com>
2021-02-14 13:08:29 -0600

    stuff!

---
+++ Assets/AnimalMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        if (count > 1000)
+        {
+            count = 0;
+        }
+        var Vec = transform.localPosition;  
+        Vec.x += count > 500 ? .01f : -.01f;
+        //Vec.y += count > 400 ? .2f : -.2f;
+        transform.localPosition = Vec;  
+        
+
+    }
+}
